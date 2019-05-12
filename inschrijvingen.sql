814f30bf1e291b8978953e54b522114058575cf0
FrederikCoster <47323204+FrederikCoster@users.noreply.github.com>
2019-05-12 15:16:46 +0100

    Update inschrijvingen.sql

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,7 +1,3 @@
-    INSERT INTO 
-    klassen_studenten(ov_nummer, klas_code) 
-    VALUES
-	(90001,'lo8e-amo1'),
-	(90002,'lo8e-amo2'),
-	(90003,'lo8e-amo3'),
-	(90004,'lo8e-amo4');
+    UPDATE studenten
+    SET geboortedatum= '2001-05-20'
+    WHERE geboortedatum= '2002-05-20';
