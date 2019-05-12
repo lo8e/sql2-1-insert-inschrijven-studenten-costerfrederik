c72e35f1f042f712e1ceea0569e695346f0d395c
FrederikCoster <47323204+FrederikCoster@users.noreply.github.com>
2019-05-12 15:08:39 +0100

    Update inschrijvingen.sql

---
+++ inschrijvingen.sql
@@ -0,0 +1,8 @@
+USE `lo8e_sql2`;
+INSERT INTO studenten 
+(ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum)
+VALUES
+	(90001,'Kriesels','','Maren','2002-05-20','2231 SK','Leiden', 73.2, 2.10, '2018-01-01'),
+	(90002,'Lobke','','Huigen','1998-08-10','2132 GH','Zoetermeer', 82.1, 1.80, '2018-01-01'),
+	(90003,'Seijkens','','Romke','1996-03-20','3782 PK','Katwijk', 67.0, 1.70, '2018-01-01'),
+	(90004,'Bruggeman','van','Poonam','1993-07-14','8392 XT','Leiden', 72.5, 1.85, '2018-01-01');
